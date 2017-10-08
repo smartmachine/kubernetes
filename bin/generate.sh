ef31f10fde9b579fe2a46249ba546805cc14f81a
Conrad Christensen <conrad.christensen@gmail.com>
2017-10-08 21:44:12 +0200

    Updated

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+set -o pipefail
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   BINPATH=bin/linux
 elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -10,7 +13,7 @@ else
 fi
 
 rm -rf config/bootkube
-${BINPATH}/bootkube render --asset-dir config/bootkube --api-servers https://cluster.kube.com:443 --api-server-alt-names=DNS=cluster.kube.com --etcd-servers https://master-1.kube.com:2379 --pod-cidr 10.2.0.0/16 --service-cidr 10.3.0.0/16 --experimental-calico-network-policy
+${BINPATH}/bootkube render --asset-dir config/bootkube --api-servers https://cluster.kube.com:443 --api-server-alt-names=DNS=cluster.kube.com --etcd-servers https://master-1.kube.com:2379 --pod-cidr 10.2.0.0/16 --service-cidr 10.3.0.0/16 --network-provider experimental-calico
 
 export SAN=DNS.1:matchbox.kube.com,IP.1:192.168.99.2
 
